f7048997384fabf7d476dd7798bce21b0d1bba6c
Benalgins <simeon.ivanew@gmail.com>
2024-08-13 22:17:29 +0300

    Adjusting CSS on Home and login pages

--- TB-Reviews/src/components/Home/Home.jsx
+++ TB-Reviews/src/components/Home/Home.jsx
@@ -1,7 +1,20 @@
 export default function Home() {
 	return (
-		<div className="container">
-			<h1>Here you can check our recepies!</h1>
-		</div>
+		<>
+			<div className="container">
+				<h1>Wellcome to Hookah Tobacco Reviews!</h1>
+			</div>
+			<article>
+				<p>
+					Welcome to Hookah Tobacco Reviews, your go-to platform for sharing and
+					discovering honest opinions on various tobacco products. Sign in to
+					post your reviews, explore feedback from fellow enthusiasts, and
+					engage in discussions that help you find the perfect blend. Our
+					community-driven site is dedicated to providing insightful
+					perspectives, making it easier for everyone to enjoy the best hookah
+					experience. Join us today and contribute to the conversation.{' '}
+				</p>
+			</article>
+		</>
 	);
 }
